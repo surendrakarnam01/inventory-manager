2e4afff1b23264092acc5d1a8240f268f64f0aac
unknown <surendrakarnam01@gmail.com>
2026-03-31 15:07:11 +0530

    Force db create_all on startup

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+with app.app_context:
+    db.create_all()
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
